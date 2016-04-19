ad5f17b80948113d0e4b06348029fe048817222d
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-18 22:55:56 -0700

    gulp and closure working

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var closureCompiler = require('google-closure-compiler').gulp();
+ 
+gulp.task('default', function () {
+  return gulp.src('./src/**/*.js', {base: './'})
+      .pipe(closureCompiler({
+          compilation_level: 'SIMPLE',
+          warning_level: 'VERBOSE',
+          language_in: 'ECMASCRIPT6_STRICT',
+          language_out: 'ECMASCRIPT5_STRICT',
+          output_wrapper: '(function(){\n%output%\n}).call(this)',
+          js_output_file: 'output.min.js'
+        }))
+      .pipe(gulp.dest('./dist'));
+});
